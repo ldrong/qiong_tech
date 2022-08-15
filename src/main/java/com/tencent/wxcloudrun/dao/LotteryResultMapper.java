a632620584dc2793e0fadc3fb3e2242b9323ca39
dingrongliu <932420839@qq.com>
2022-08-16 00:05:11 +0800

    modify Mapper

--- src/main/java/com/tencent/wxcloudrun/dao/LotteryResultMapper.java
+++ src/main/java/com/tencent/wxcloudrun/dao/LotteryResultMapper.java
@@ -2,9 +2,10 @@ package com.tencent.wxcloudrun.dao;
 
 import com.tencent.wxcloudrun.model.LotteryResult;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface LotteryResultMapper {
-    public LotteryResult getLotteryResult(String lotteryId, long lotteryNo);
-    public LotteryResult getLotteryResult(String lotteryId);
+    public LotteryResult getLotteryResult(@Param("lotteryId") String lotteryId, @Param("lotteryNo") long lotteryNo);
+    public LotteryResult getLotteryResult(@Param("lotteryId") String lotteryId);
 }
